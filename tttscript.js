476dd95dfe6d62f32264389c1629625bbc81c02b
ZakSchenck <91504668+ZakSchenck@users.noreply.github.com>
2021-11-02 14:22:50 -0400

    Added instagram link to nav, and a Name textarea in contact

--- tttscript.js
+++ tttscript.js
@@ -6,12 +6,29 @@ window.addEventListener('scroll', () => {
 
 let hamburger = document.querySelector('.hamburger');
 let listContainer = document.querySelector('.list-container');
+let dropdownButton = document.querySelector('#dropdown-button');
+let dropdownMenu = document.querySelector('.dropdown-menu');
 
 hamburger.addEventListener('click', () => {
-  listContainer.classList.toggle('show')
-  listContainer.classList.toggle('nav-is-open')
+  listContainer.classList.toggle('show');
 });
 
 function myFunction(x) {
   x.classList.toggle("change");
+}
+
+
+
+for(let i = 1; i <= 100; i++) {
+  let output = '';
+  if(i % 3 == 0) {
+    output += ('Fizz');
+  }
+  if(i % 5 == 0) {
+    output += ('Buzz');
+  }
+  if(output == ''){
+    output = i
+  }
+  console.log(output)
 }
